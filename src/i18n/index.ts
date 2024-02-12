c980a3a17629c7176e7808436bd5f039b3d1e5fb
Gabi <galvarez@pelmorex.com>
2024-02-12 08:56:22 +0100

    Add Button component and update i18n for video section

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,7 +5,7 @@ const LANGUAGES = {
     CATALAN: 'ca',
     SPANISH: 'es'
 }
-export const getI18N = ({ currentLocale = 'es' } : { currentLocale?: string | undefined }) => {
+export const getI18N = ({ currentLocale = 'es' } : { currentLocale?: string | 'es' }) => {
     if (currentLocale === LANGUAGES.CATALAN) {
         return catalan;
     }
